ada39a0ee73ee21abe20126a8a8eb8a1ed2665eb
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2021-08-22 17:40:15 -0400

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5c95720e41c0cd35fa6c665dd0222502'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9b86c78b-8a19-46d4-8e45-a93714c70107'
+};
